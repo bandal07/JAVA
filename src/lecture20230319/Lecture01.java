3e4794838d4e57953bd3c30a51512bb8dbdce798
bandal07 <bandal0796@gmail.com>
2023-03-19 14:09:01 +0900

    ãjava수정

---
+++ src/lecture20230319/Lecture01.java
@@ -0,0 +1,23 @@
+package lecture20230319;
+
+public class Lecture01 {
+    public static void main(String[] args) {
+
+        byte n1 = 1;
+        short n2 = 2;
+        int n3 = 3;
+        long n4 = 4L;
+
+        float fn = 11f;
+        double dn = 22.0;
+
+        char character = 'A';
+
+        boolean flag = true;
+
+
+        String str = "HI";
+
+
+    }
+}
