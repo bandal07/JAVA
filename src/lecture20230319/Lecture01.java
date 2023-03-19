e69bb57cd6e5efd22085f184d16e9e1534fc9be5
bandal07 <bandal0796@gmail.com>
2023-03-19 14:12:56 +0900

    추가수정

--- src/lecture20230319/Lecture01.java
+++ src/lecture20230319/Lecture01.java
@@ -2,22 +2,28 @@ package lecture20230319;
 
 public class Lecture01 {
     public static void main(String[] args) {
-
+        //기본형(primitive type)
+        
+        //정수형
         byte n1 = 1;
         short n2 = 2;
         int n3 = 3;
         long n4 = 4L;
 
+        //실수형
         float fn = 11f;
         double dn = 22.0;
 
+        //문자형
         char character = 'A';
 
+        //boolean형
         boolean flag = true;
-
-
+        
+        //참조형(reference type)
         String str = "HI";
 
 
+        //사용자 정의 타입
     }
 }
