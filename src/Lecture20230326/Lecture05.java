2d2ea7953ecf174839a3ffe7baf3410b4438222a
bandal07 <bandal0796@gamil.com>
2023-03-26 14:00:34 +0900

    테스트

---
+++ src/Lecture20230326/Lecture05.java
@@ -0,0 +1,7 @@
+package Lecture20230326;
+
+public class Lecture05 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/lecture20230319/Test.java
+++ src/lecture20230319/Test.java
@@ -2,6 +2,10 @@ package lecture20230319;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.print("HI");
+        for (int i = 2; i <= 9; i++){
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("i j ="+ i * j);
+            }
+        }
     }
 }
